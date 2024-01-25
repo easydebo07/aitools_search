00eda07f8bd780b0e9b4f1ceffd97ee9bdaac232
easydebo07 <153956520+easydebo07@users.noreply.github.com>
2024-01-24 18:57:43 -0600

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,16 +2,16 @@
 // set system variables
 
 // api endpoint to send search to
-$search_endpoint = "http://54.89.247.176:8080/search";
+$search_endpoint = "http://3.34.234.122:8080/search";
 
 // category endpoint: data for categories
-$category_endpoint = "http://54.89.247.176:8080/category_group";
+$category_endpoint = "http://3.34.234.122:8080/category_group";
 
 // Recent Search endpoint: data for recently viewed or search
-$recent_search_endpoint = "http://54.89.247.176:8080/recent_search";
+$recent_search_endpoint = "http://3.34.234.122:8080/recent_search";
 
 // url to snd new members to register and add new tools
-$member_endpoint = "http://54.89.247.176:8090/";
+$member_endpoint = "http://3.34.222.121:8090/";
 
 // amount of search result to display
 $listNumber = 5;
